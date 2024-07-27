e31660e1f78b5b83c957894c43e5c0feba636008
Touqeer Admed <touqeerahmed1633@gmail.com>
2024-07-28 02:51:39 +0500

    wip auth usrt

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+import asyncHandler from "express-async-handler";
+
+// @desc Auth user/set token
+// route POST /api/users/auth
+// @access Public
+
+const authUser =  asyncHandler((req, res) => {
+    res.status(200).json({ message: "Auth user" });
+})
+
+export { authUser}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config();
 
-const port = 5001;
+const port = process.env.PORT || 5000;
 
+import userRoutes from './routes/userRoutes.js' 
 const app = express();
+
+app.use('/api/users/', userRoutes)
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+
+// - **POST /api/users** - Register a user
+// - **POST /api/users/auth** - Authenticate a user and get token
+// - **POST /api/users/logout** - Logout user and clear cookie
+// - **GET /api/users/profile** - Get user profile
+// - **PUT /api/users/profile** - Update user
