7508b014482e9cb6c8a78d114962219258283346
Touqeer Ahmed <touqeer.ahmed@retrocube.org>
2024-07-31 12:11:21 +0500

    database connected

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,71 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
-const authUser =  asyncHandler((req, res) => {
+const authUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Auth user" });
+});
 
-    res.status(401)
-    throw new Error('Something went wrong!')
+// @desc Register a new user
+// route POST /api/users
+// @access Public
 
-    res.status(200).json({ message: "Auth user" });
-})
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-export { authUser}
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email already exists.");
+  }
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  //   res.status(200).json({ message: "Register user" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+
+const logoutUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Logout user" });
+});
+
+// @desc Get user profile
+// route GET /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc Update user profile
+// route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
-import express from 'express';
-import dotenv from 'dotenv'
-dotenv.config();
+import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 
-import userRoutes from './routes/userRoutes.js' 
-import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+connectDB();
+
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users/', userRoutes)
-
-app.get('/', (req, res) => res.send('Server is ready'));
+app.use("/api/users/", userRoutes);
+app.get("/", (req, res) => res.send("Server is ready"));
 app.use(notFound);
 app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
 
 // - **POST /api/users** - Register a user
 // - **POST /api/users/auth** - Authenticate a user and get token
